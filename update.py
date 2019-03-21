134b3dcf7df838a8783173f98670affb2effef61
Fenhl <fenhl@fenhl.net>
2019-03-21 12:20:17 +0000

    Update to use a newer Twitch API

--- update.py
+++ update.py
@@ -2,12 +2,24 @@
 
 import sys
 
+import basedir
 import datetime
 import json
+import more_itertools
 import pathlib
 import re
 import requests
 import sleeptill
+import time
+
+CACHE = {
+    'client_id': None
+}
+
+def client_id():
+    if CACHE['client_id'] is None:
+        CACHE['client_id'] = basedir.config_dirs('info-beamer-twitch-chat.json').json()['clientID']
+    return CACHE['client_id']
 
 def format_timedelta(timestamp):
     minutes, seconds = divmod(timestamp.total_seconds(), 60)
@@ -15,28 +27,42 @@ def format_timedelta(timestamp):
     return '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds))
 
 def format_message(message):
-    a = message['attributes']
     return [
-        format_timedelta(datetime.timedelta(milliseconds=a['video-offset'])),
-        '{}:'.format(a['from'])
-    ] + a['message'].split(' ')
+        format_timedelta(datetime.timedelta(seconds=message['content_offset_seconds'])),
+        '{}:'.format(message['commenter']['display_name'])
+    ] + message['message']['body'].split(' ') #TODO use fragments instead and apply formatting
+
+def get_json(*args, headers={}, **kwargs):
+    response = requests.get(*args, headers={'Client-ID': client_id(), **headers}, **kwargs)
+    response.raise_for_status()
+    return response.json()
+
+def parse_duration(duration_str):
+    total = datetime.timedelta()
+    while duration_str:
+        match = re.match('([0-9]+[hms])', duration_str)
+        if not match:
+            return total + datetime.timedelta(seconds=float(duration_str))
+        part = match.group(1)
+        num = int(part[:-1])
+        unit = part[-1]
+        total += datetime.timedelta(**{{'h': 'hours', 'm': 'minutes', 's': 'seconds'}[unit]: num})
+        duration_str = duration_str[len(part):]
+    return total
 
 def replay(video_id, start=datetime.timedelta(), *, max_lines=50, out=pathlib.Path('data.json')):
     replay_start = datetime.datetime.now(datetime.timezone.utc) - start
-    video_id_str = 'v{}'.format(video_id)
-    j = requests.get('https://rechat.twitch.tv/rechat-messages', params={'video_id': video_id_str, 'start': '0'}).json()
-    match = re.fullmatch('0 is not between ([0-9]+) and ([0-9]+)', j['errors'][0]['detail'])
-    if not match:
-        raise RuntimeError(j['errors'][0]['detail'])
-    video_len = (int(match.group(2)) - int(match.group(1)))
+    #vod_info = get_json('https://api.twitch.tv/helix/videos', params={'id': video_id})
+    #video_len = parse_duration(more_itertools.one(vod_info['data'])['duration'])
+    j = get_json('https://api.twitch.tv/v5/videos/{}/comments'.format(video_id), params={'content_offset_seconds': start.total_seconds()})
     data = []
-    for offset_secs in range((start // datetime.timedelta(seconds=30)) * 30, video_len, 30):
-        offset = datetime.timedelta(seconds=offset_secs)
-        j = requests.get('https://rechat.twitch.tv/rechat-messages', params={'video_id': video_id_str, 'offset_seconds': str(offset_secs)}).json()
-        for message in j['data']:
-            if message['type'] != 'rechat-message':
-                raise ValueError('Unknown message type: {}'.format(message['type']))
-            msg_offset = datetime.timedelta(milliseconds=message['attributes']['video-offset'])
+    while j is not None:
+        for message in j['comments']:
+            if message['state'] != 'published':
+                raise ValueError('Unknown message state: {}'.format(message['state']))
+            if message['more_replies']:
+                raise ValueError('Not sure what message["more_replies"] does')
+            msg_offset = datetime.timedelta(seconds=message['content_offset_seconds'])
             if msg_offset < start:
                 continue
             data.append(format_message(message))
@@ -46,10 +72,16 @@ def replay(video_id, start=datetime.timedelta(), *, max_lines=50, out=pathlib.Pa
             with out.open('w') as out_f:
                 json.dump(data, out_f, indent=4, sort_keys=True)
                 print(file=out_f)
+        if '_next' in j:
+            j = get_json('https://api.twitch.tv/v5/videos/{}/comments'.format(video_id), params={'cursor': j['_next']})
+        else:
+            j = None
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
-        start = datetime.timedelta(seconds=int(sys.argv[2]))
+        start = parse_duration(sys.argv[2])
     else:
         start = datetime.timedelta()
     replay(sys.argv[1], start)
+    while True:
+        time.sleep(1) # keep script alive so info-beamer can be quit using ^C
